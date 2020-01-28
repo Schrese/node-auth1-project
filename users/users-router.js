dd3edb084d0d2a3b88fd2ae4df17388744a2ddfd
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-28 15:30:46 -0500

    get request is now protected

--- users/users-router.js
+++ users/users-router.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const router = require('express').Router();
 
 const Users = require('./users-model.js');
+const restricted = require('../auth/restricted-middleware.js')
 
-const router = express.Router();
- 
-router.get('/', (req, res) => {
+router.get('/', restricted, (req, res) => {
     Users.getUsers()
         .then(users => {
             res.status(200).json(users);
